0edd9deb4ad0e4198d9ad076d3935e7f68ebc18a
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-05-09 15:55:55 +0800

    Add files via upload

---
+++ MyPlatForm/UI界面文件/Fault_Monitoring_Platform/Fault_Monitoring_Platform_zh_CN.ts
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="Fault_Monitoring_Platform_zh_CN"></TS>
---
+++ MyPlatForm/UI界面文件/Fault_Monitoring_Platform/main.cpp
@@ -0,0 +1,11 @@
+#include "faultPlatform.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    faultPlatform w;
+    w.show();
+    return a.exec();
+}
